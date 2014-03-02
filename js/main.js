7818ca3ec6d094b03b140b89679e59f8f5cd772d
Dmitry Savenko <dsavenko@xored.com>
2014-03-02 22:22:06 +0700

    Contact info

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
-jQuery(document).ready(function () {
+var fillDsMl = function () {
+    var mHref = ['m', 'a', 'i', 'l', 't', 'o', ':'];
+    var mUser = ['d', 's', 'a', 'v', 'e', 'n', 'k', 'o'];
+    var mDomain = ['@', 'g', 'm', 'a', 'i', 'l', '.', 'c', 'o', 'm'];
 
+    var ml = mUser.join("") + mDomain.join("");
+    $("#ds-ml").attr({
+        href: mHref.join("") + ml,
+        target: "_self"
+    });
+
+};
+
+var makeLinksOpenInNewTab = function () {
     $("a").attr("target", "_blank");
+};
 
+jQuery(document).ready(function () {
+    makeLinksOpenInNewTab();
+    fillDsMl();
 });
